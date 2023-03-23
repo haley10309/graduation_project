ddb056f2538c9645efea67bc8b4366f7bbcc1699
DaEun Lee <dlekdms1026@gmail.com>
2023-03-23 14:28:33 +0900

    react

--- servlet/src/main/frontend/src/App.js
+++ servlet/src/main/frontend/src/App.js
@@ -1,41 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from "react";
+import Login from "./Login"
 
 function App() {
-    const [message, setMessage] = useState([]);
 
-    useEffect(() => {
-        fetch("/hello")
-            .then((response) => {
-                return response.json();
-            })
-            .then(function (data) {
-                setMessage(data);
-            });
-    }, []);
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <ul>
-                    {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
-                </ul>
-            </header>
-        </div>
-    );
+  return (
+    <div >
+      <Login/>
+    </div>
+  );
+
+
 }
 
 export default App;
