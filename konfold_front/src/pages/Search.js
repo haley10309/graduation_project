d6bee05b476568d277f090140a82441491c003c6
hayung <yoohayung10309@gmail.com>
2023-05-04 17:24:07 +0900

    search certain condition (검색 조건)

--- konfold_front/src/pages/Search.js
+++ konfold_front/src/pages/Search.js
@@ -12,9 +12,22 @@ export default function Search(){
     
   const [protein, setProtein] = useState(""); //입력 값 변수 [입력값, 입력값 변경]
   const [proteinName, setProteinName] = useState(""); // 저장 후 변수
+
+  const [button, setButton] = useState(true);
+
+  function changeButton(){
+    const UpperProtein = protein.toUpperCase();
+   
+    UpperProtein.includes('G' ||'A'||'V'||'L'||'I'||'S'||'T'||'C'||'M'||'D'||'E'||'N'||'Q'||'K'||'R'||'F'||'Y'||'W'||'H'||'P'||'U') ? setButton(false) : setButton(true)
+  
+   }
+
+
   const handleInput = (event) => {
+    event.preventDefault();
     setProtein(event.target.value); //변수 저장 완료
-    
+    // const UpperProtein = protein.toUpperCase();
+    // UpperProtein.includes('G' ||'A'||'V'||'L'||'I'||'S'||'T'||'C'||'M'||'D'||'E'||'N'||'Q'||'K'||'R'||'F'||'Y'||'W'||'H'||'P'||'U') ? setButton(false) : setButton(true)
   };
   
   
@@ -26,14 +39,18 @@ export default function Search(){
 
     //window.location.href = "/proteinInput";
 
-   
-    axios.get('/api/Input',
+    // const result = await axios.get('http://127.0.0.1:5000/api/Input');
+    // this.recordCount = result.headers["x-totalrecordcount"];
+
+    axios.get('http://127.0.0.1:5000/api/Input',
       {params: { "proteinName" : protein }
     }).then(function (response) {
       console.log(protein);
      }).catch(function (error){
       console.log(error);
      })
+
+
      axios.post('/api/Input',{
       proteinName: protein
      }).then(function(response){
@@ -65,6 +82,8 @@ export default function Search(){
             className="input"
             value={protein} //input으로 받은 프로틴 시퀀스
             onChange={handleInput}
+            onKeyUp={changeButton}
+            
           />
 
           {/* place holder 넣어 보기 */}
@@ -73,7 +92,10 @@ export default function Search(){
         <div className="errorMessageWrap">올바른 시퀀스를 입력해 주세요</div>
       </div>
       <div>
-        <button disabled={false} onClick={confirm} className="bottomButton">
+        <button 
+        disabled={button} 
+        onClick={confirm} 
+        className="bottomButton">
           확인
         </button>
       </div>
