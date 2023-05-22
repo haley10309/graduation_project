d085bf991e24219c9268e39ea5586069ca85241d
Dang <dlekdms1026@naver.com>
2023-05-22 20:52:14 +0900

    new visual

--- konfold_front/src/pages/Refer.jsx
+++ konfold_front/src/pages/Refer.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Component } from 'react';
 import { BrowserRouter,Route, Routes } from "react-router-dom";
 
-import Prediction from "/Users/hayungyoo/project/konfold_front/src/pages/Prediction.jsx";
-import Pred_mech from "/Users/hayungyoo/project/konfold_front/src/pages/Pred_mech.jsx";
-import Pred_skills from "/Users/hayungyoo/project/konfold_front/src/pages/Pred_skills.jsx";
-import Pred_ability from "/Users/hayungyoo/project/konfold_front/src/pages/Pred_ability.jsx";
+import Prediction from "/Users/win10-aa/Desktop/c_e/graduation_project/konfold_front/src/pages/Prediction.jsx";
+import Pred_mech from "/Users/win10-aa/Desktop/c_e/graduation_project/konfold_front/src/pages/Pred_mech.jsx";
+import Pred_skills from "/Users/win10-aa/Desktop/c_e/graduation_project/konfold_front/src/pages/Pred_skills.jsx";
+import Pred_ability from "/Users/win10-aa/Desktop/c_e/graduation_project/konfold_front/src/pages/Pred_ability.jsx";
 
 
 
